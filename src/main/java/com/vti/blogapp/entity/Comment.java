637d271a3cbfc988086467afab7c1bf48956006e
Phuc776 <doxphuc@gmail.com>
2024-07-19 11:47:05 +0700

    Update

--- src/main/java/com/vti/blogapp/entity/Comment.java
+++ src/main/java/com/vti/blogapp/entity/Comment.java
@@ -14,8 +14,17 @@ import java.time.LocalDateTime;
 public class Comment {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(
+            name = "comment_id_generator",
+            sequenceName = "comment_id_sequence",
+            initialValue = 99,
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "comment_id_sequence")
     private Long id;
+
     @Column(name = "name", length = 50, nullable = false)
     private String name;
     @Column(name = "email", length = 75, nullable = false)
